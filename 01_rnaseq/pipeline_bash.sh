6ec6bbf2617c83bc1ef69f298ee682d0585743fd
pierpaolo-cantisani <pierpaolo.cantisani@gmail>
2026-04-01 23:09:40 +0200

    Modifying .sh files

--- 01_rnaseq/pipeline_bash.sh
+++ 01_rnaseq/pipeline_bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ### 1. Downloading and merging files ###
 # SRR codes are in "SRR_Acc_List.txt", obtained from the SRA website.
@@ -9,40 +10,40 @@ while read id; do
 
   #Downloading
   prefetch "$id"
-  fasterq-dump "$id" --outdir ~/multi_omics/rna_seq/fastq
+  fasterq-dump "$id" --outdir ~/Multi-omics-analysis/01_rnaseq/fastq
   #Compressing
-  gzip "$HOME/multi_omics/rna_seq/fastq/${id}.fastq"
+  gzip "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${id}.fastq"
 
   # Removing the prefetch directory
   rm -r "$id"
-done < ~/multi_omics/rna_seq/SRR_Acc_List.txt
+done < ~/Multi-omics-analysis/01_rnaseq/SRR_Acc_List.txt
 
 
 # Merging
 #The experiment is composed of 48 runs, 4 for each of 12 samples. Merging each quadruple of files:
-mapfile -t srr_codes < ~/multi_omics/rna_seq/SRR_Acc_List.txt
+mapfile -t srr_codes < ~/Multi-omics-analysis/01_rnaseq/SRR_Acc_List.txt
 
 for ((i=0; i<${#srr_codes[@]}; i+=4)); do
    j=$((i/4 + 1))
    echo "Merging sample $j"
    cat \
-      "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i]}.fastq.gz" \
-      "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i+1]}.fastq.gz" \
-      "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i+2]}.fastq.gz" \
-      "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i+3]}.fastq.gz" \
-      > "$HOME/multi_omics/rna_seq/fastq/sample_${j}_merged.fastq.gz"
+      "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i]}.fastq.gz" \
+      "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i+1]}.fastq.gz" \
+      "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i+2]}.fastq.gz" \
+      "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i+3]}.fastq.gz" \
+      > "$HOME/Multi-omics-analysis/01_rnaseq/fastq/sample_${j}_merged.fastq.gz"
   
    #Removing non-merged files
-   rm "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i]}.fastq.gz" "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i+1]}.fastq.gz" "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i+2]}.fastq.gz" "$HOME/multi_omics/rna_seq/fastq/${srr_codes[i+3]}.fastq.gz"
+   rm "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i]}.fastq.gz" "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i+1]}.fastq.gz" "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i+2]}.fastq.gz" "$HOME/Multi-omics-analysis/01_rnaseq/fastq/${srr_codes[i+3]}.fastq.gz"
 done
 
 
 ### 2. Quality Control
 
 #Performing QC:
-fastqc -t 2 -o ~/multi_omics/rna_seq/fastqc_results/not_trimmed ~/multi_omics/rna_seq/fastq/*.fastq.gz
+fastqc -t 4 -o ~/Multi-omics-analysis/01_rnaseq/fastqc_results/not_trimmed ~/Multi-omics-analysis/01_rnaseq/fastq/*.fastq.gz
 #Merging QC results:
-multiqc ~/multi_omics/rna_seq/fastqc_results/not_trimmed -n full_qc_report -o ~/multi_omics/rna_seq/fastqc_results/not_trimmed
+multiqc ~/Multi-omics-analysis/01_rnaseq/fastqc_results/not_trimmed -n full_qc_report_not_trimmed -o ~/Multi-omics-analysis/01_rnaseq/fastqc_results/
 
 
 ### 3. Trimming and second Quality Control
@@ -50,50 +51,50 @@ for ((i=1; i<13; i+=1)); do
   echo "Trimming sample: $i"
 
   fastp \
-    -t 2 \
-    --in1 "$HOME/multi_omics/rna_seq/fastq/sample_${i}_merged.fastq.gz" \
-    --out1 "$HOME/multi_omics/rna_seq/fastq_trimmed/sample_${i}_trimmed.fastq.gz" \
+    -w 4 \
+    --in1 "$HOME/Multi-omics-analysis/01_rnaseq/fastq/sample_${i}_merged.fastq.gz" \
+    --out1 "$HOME/Multi-omics-analysis/01_rnaseq/fastq_trimmed/sample_${i}_trimmed.fastq.gz" \
+    --html "$HOME/Multi-omics-analysis/01_rnaseq/fastqc_results/trimmed/sample_${i}_fastp.html" \
+    --json "$HOME/Multi-omics-analysis/01_rnaseq/fastqc_results/trimmed/sample_${i}_fastp.json" \
     --qualified_quality_phred 20 \
     --length_required 40
 
-  rm "$HOME/multi_omics/rna_seq/fastq/sample_${i}_merged.fastq.gz"
+  rm "$HOME/Multi-omics-analysis/01_rnaseq/fastq/sample_${i}_merged.fastq.gz"
 
 done
 
 #Performing QC:
-fastqc -t 2 -o ~/multi_omics/rna_seq/fastqc_results/trimmed ~/multi_omics/rna_seq/fastq_trimmed/*.fastq.gz
+fastqc -t 4 -o ~/Multi-omics-analysis/01_rnaseq/fastqc_results/trimmed ~/Multi-omics-analysis/01_rnaseq/fastq_trimmed/*.fastq.gz
 #Merging QC results:
-multiqc ~/multi_omics/rna_seq/fastqc_results/trimmed -n full_qc_report_trimmed -o ~/multi_omics/rna_seq/fastqc_results/trimmed
+multiqc ~/Multi-omics-analysis/01_rnaseq/fastqc_results/trimmed -n full_qc_report_trimmed -o ~/Multi-omics-analysis/01_rnaseq/fastqc_results/
 
 
 
 ### 4. Mapping and quantification ###
 ##Using Salmon
 
-mkdir -p ~/multi_omics/rna_seq/salmon_output
-
 for ((i=1; i<13; i+=1)); do
   echo "Quantifying sample: ${i}"
 
   salmon quant -l A \
-        -i ~/references/salmon_index_hg38_49 \
-        -r "$HOME/multi_omics/rna_seq/fastq_trimmed/sample_${i}_trimmed.fastq.gz" \
-        -p 2 \
+        -i ~/Multi-omics-analysis/references/salmon_index_hg38_49 \
+        -r "$HOME/Multi-omics-analysis/01_rnaseq/fastq_trimmed/sample_${i}_trimmed.fastq.gz" \
+        -p 4 \
         --validateMappings \
-        -o "$HOME/multi_omics/rna_seq/salmon_output/sample_${i}_quant"
+        -o "$HOME/Multi-omics-analysis/01_rnaseq/salmon_output/sample_${i}_quant"
 
 done
 
-#Check: Statistics and file format
-less ~/multi_omics/rna_seq/salmon_output/SRR1725922_quant/lib_format_counts.json
-head -n 20 ~/multi_omics/rna_seq/salmon_output/SRR1725922_quant/quant.sf | column -t | less
+#At this point it's important to check the results: statistics and file format
+#less ~/Multi-omics-analysis/01_rnaseq/salmon_output/sample_1_quant/lib_format_counts.json
+#head -n 20 ~/Multi-omics-analysis/01_rnaseq/salmon_output/sample_1_quant/quant.sf | column -t | less
 
 #Renaming directories for differential analysis:
 n=1
 for ((i=1; i<13; i+=2)); do
 
-  mv "$HOME/multi_omics/rna_seq/salmon_output/sample_${i}_quant" "$HOME/multi_omics/rna_seq/salmon_output/rep_${n}_TB_quant"
-  mv "$HOME/multi_omics/rna_seq/salmon_output/sample_$((i+1))_quant" "$HOME/multi_omics/rna_seq/salmon_output/rep_${n}_NI_quant"
+  mv "$HOME/Multi-omics-analysis/01_rnaseq/salmon_output/sample_${i}_quant" "$HOME/Multi-omics-analysis/01_rnaseq/salmon_output/rep_${n}_TB_quant"
+  mv "$HOME/Multi-omics-analysis/01_rnaseq/salmon_output/sample_$((i+1))_quant" "$HOME/Multi-omics-analysis/01_rnaseq/salmon_output/rep_${n}_NI_quant"
   ((n++))
 
 done
--- setup.sh
+++ setup.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
+set -e
 
 ### Creating conda environment ###
 
-conda create -n project_env python=3.10 
-
-conda activate project_env
-
-conda install -c bioconda -c conda-forge \
-  sra-tools \
-  fastqc \
-  multiqc \
-  fastp \
-  salmon
-
-conda deactivate
-
+conda env create -f ~/Multi-omics-analysis/01_rnaseq/rna_seq.yml
 
 
 ### Creating directories ###
-
-mkdir -p ~/multi_omics/rna_seq
-mkdir -p ~/multi_omics/rna_seq/fastq
-mkdir -p ~/multi_omics/rna_seq/fastqc_results/not_trimmed
-mkdir -p ~/multi_omics/rna_seq/fastq_trimmed
-mkdir -p ~/multi_omics/rna_seq/fastqc_results/trimmed
-mkdir -p ~/multi_omics/rna_seq/salmon_output
-mkdir -p ~/references
-
+mkdir -p ~/Multi-omics-analysis/01_rnaseq/{fastq,fastqc_results/not_trimmed,fastq_trimmed,fastqc_results/trimmed,salmon_output}
+mkdir -p ~/Multi-omics-analysis/references
 
 
 ### Downloading and indexing reference transcriptome for salmon quantification ###
 
 #Downloading
-wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_49/gencode.v49.transcripts.fa.gz
+wget -P ~/Multi-omics-analysis/references/ \
+  https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_49/gencode.v49.transcripts.fa.gz
 
 # Indexing
-salmon index -t ~/references/gencode.v49.transcripts.fa.gz -i ~/references/salmon_index_hg38_49
+salmon index \
+  -t ~/Multi-omics-analysis/references/gencode.v49.transcripts.fa.gz \
+  -i ~/Multi-omics-analysis/references/salmon_index_hg38_49 \
+  --threads 4
